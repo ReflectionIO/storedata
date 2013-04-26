00de48be50a57cbffa9764482741e0a4ec9115d1
billy1380 <billy1380@fcbadaa9-13bc-4346-a2d3-50fbbe9e6b1b>
2013-04-26 10:52:08 +0000

    added rank count map reduce

---
+++ src/com/spacehopperstudios/storedatacollector/mapreduce/RankCountReducer.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package com.spacehopperstudios.storedatacollector.mapreduce;
+
+import org.apache.log4j.Logger;
+
+import com.google.appengine.tools.mapreduce.KeyValue;
+import com.google.appengine.tools.mapreduce.Reducer;
+import com.google.appengine.tools.mapreduce.ReducerInput;
+
+/**
+ * @author William Shakour
+ * 
+ */
+public class RankCountReducer extends Reducer<String, Long, KeyValue<String, Long>> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private static final Logger LOG = Logger.getLogger(RankCountReducer.class);
+
+	private void emit(String key, long outValue) {
+		// log.info("emit(" + outValue + ")");
+		getContext().emit(KeyValue.of(key, outValue));
+	}
+
+	@Override
+	public void beginShard() {
+	}
+
+	@Override
+	public void beginSlice() {
+	}
+
+	@Override
+	public void reduce(String key, ReducerInput<Long> values) {
+		// log.info("reduce(" + key + ", " + values + ")");
+		long total = 0;
+		while (values.hasNext()) {
+			long value = values.next();
+			total += value;
+		}
+		emit(key, total);
+	}
+
+	@Override
+	public void endShard() {
+	}
+
+	@Override
+	public void endSlice() {
+	}
+
+}
