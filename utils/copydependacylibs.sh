f49f209473cca9cf1a4428046085e92195646c9c
billy1380 <billy1380@fcbadaa9-13bc-4346-a2d3-50fbbe9e6b1b>
2013-06-27 12:45:05 +0000

    added shell script to move maven obtained dependencies to libs folder

---
+++ utils/copydependacylibs.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+cp ~/.m2/repository/com/google/appengine/appengine-api-1.0-sdk/1.8.1/appengine-api-1.0-sdk-1.8.1.jar ~/Documents/EclipseProjects/storedatacollector/war/WEB-INF/lib
+cp ~/.m2/repository/com/google/appengine/tools/appengine-mapreduce/0.2/appengine-mapreduce-0.2.jar ~/Documents/EclipseProjects/storedatacollector/war/WEB-INF/lib
+cp ~/.m2/repository/com/google/appengine/tools/appengine-gcs-client/0.3/appengine-gcs-client-0.3.jar ~/Documents/EclipseProjects/storedatacollector/war/WEB-INF/lib
+cp ~/.m2/repository/com/google/appengine/tools/appengine-pipeline/0.1/appengine-pipeline-0.1.jar ~/Documents/EclipseProjects/storedatacollector/war/WEB-INF/lib
+cp ~/.m2/repository/com/googlecode/charts4j/charts4j/1.3/charts4j-1.3.jar ~/Documents/EclipseProjects/storedatacollector/war/WEB-INF/lib
+cp ~/.m2/repository/org/json/json/20090211/json-20090211.jar ~/Documents/EclipseProjects/storedatacollector/war/WEB-INF/lib
+cp ~/.m2/repository/com/google/guava/guava/14.0.1/guava-14.0.1.jar ~/Documents/EclipseProjects/storedatacollector/war/WEB-INF/lib
+cp ~/.m2/repository/org/easymock/easymock/3.0/easymock-3.0.jar ~/Documents/EclipseProjects/storedatacollector/war/WEB-INF/lib
+cp ~/.m2/repository/cglib/cglib-nodep/2.2/cglib-nodep-2.2.jar ~/Documents/EclipseProjects/storedatacollector/war/WEB-INF/lib
+cp ~/.m2/repository/org/objenesis/objenesis/1.2/objenesis-1.2.jar ~/Documents/EclipseProjects/storedatacollector/war/WEB-INF/lib
+cp ~/.m2/repository/com/googlecode/objectify/objectify/4.0b3/objectify-4.0b3.jar ~/Documents/EclipseProjects/storedatacollector/war/WEB-INF/lib
+cp ~/.m2/repository/com/google/appengine/appengine-api-labs/1.7.6/appengine-api-labs-1.7.6.jar ~/Documents/EclipseProjects/storedatacollector/war/WEB-INF/lib
+cp ~/.m2/repository/com/google/code/gson/gson/2.2.4/gson-2.2.4.jar ~/Documents/EclipseProjects/storedatacollector/war/WEB-INF/lib
+cp ~/.m2/repository/javax/servlet/servlet-api/2.5/servlet-api-2.5.jar ~/Documents/EclipseProjects/storedatacollector/war/WEB-INF/lib
