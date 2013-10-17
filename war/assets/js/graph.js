3a390adb735201cf33624dba4a2adb64d8337fbb
dzai3 <dzai3@fcbadaa9-13bc-4346-a2d3-50fbbe9e6b1b>
2013-10-17 12:57:34 +0000

    - Added the downloads and revenue charts and also the area graph (although it doesn't calculate properly yet)

--- war/assets/js/graph.js
+++ war/assets/js/graph.js
@@ -1,6 +1,7 @@
 
 var rangeData = [];
 var historyList = {};
+var selectedGraph = "rank";
 
 var chart;
 var chartLoaded = false;
@@ -57,7 +58,13 @@ $(".btn").click(function () {
         alert(value);
     }
     else {
-        $('#graph-container').toggle();
+        // $('#graph-container').toggle();
+
+        var itemArray = value.split("_");
+        selectedGraph = itemArray[1]; 
+
+        loadChart(historyList[selectedGraph]);
+        
     }
 
 });
@@ -100,92 +107,124 @@ function getItemsRank() {
 
                 console.log(data);
 
-                var averagesData = [];
-                // var dateData = [];
-                var minPosition = 1000;
-                var maxPosition = 20;
-                var pos = 0;
-                var nodeDate;
-                var dayOfMonth = 0; // the current day of the month. i only want to show 1 entry per day
-                var dayOfMonthOld = -1;
-
-                
-
-                $.each(data.ranks, function(index, item){    
-                    // only use the specific "type" of list e.g. topfreeapplications, topfreeipadapplications
-                    //if (item.type == type) {
-                        
-                        var nodeDate = new Date(item.date);
-                        // dateData.push(nodeDate); // this is purely for the console.log call below
-
-                        dayOfMonth = nodeDate.getDate();
-
-                        if (dayOfMonth != dayOfMonthOld) {
-                         
-                            // do things a bit differently if a top grossing app
-                            if ((type.indexOf("grossing") != -1)) {
-                                pos = item.grossingPosition;
-                            } else {
-                                pos = item.position;
-                            }
-                                
-                            var thisEntry = [item.date, pos];
-                            averagesData.push(thisEntry); // store the dates
-                            
-
-                            if (pos > maxPosition) {
-                                maxPosition = pos;
-                            }
-
-                            if (pos < minPosition) {
-                                minPosition = pos;
-                            }
-                        
-                            dayOfMonthOld = dayOfMonth;
-                        }
-                    //};       
-                });
-
-                console.log(averagesData);
-                // console.log(dateData);
-
-                maxPosition += 10;
-
-                if (minPosition < 20) {
-                    minPosition = 1;
-                }
-                else  {
-                    minPosition -= 10;
-                }
-
-                var history = {};
-                history.data = averagesData;
-                history.minPosition = minPosition;
-                history.maxPosition = maxPosition;
-
-                historyList["rank"] = history; // "rank" should be dynamic
-                console.log(historyList);
-
-                // console.log("minPosition = " + minPosition);
-                // console.log("maxPosition = " + maxPosition);
-
-                // if (chartLoaded) {
-                //     refreshChart(averagesData, minPosition, maxPosition);
-                // } else {
-                //     updateApp(data.item);
-                //     loadChart(averagesData, minPosition, maxPosition);
-                //     chartLoaded = true;
-                // }
-
-                if (!chartLoaded) {
+                 if (!chartLoaded) {
                     updateApp(data.item);
                     chartLoaded = true;
                 }
 
-                loadChart(averagesData, minPosition, maxPosition);
+                createGraphData(data, type, "rank");
+                createGraphData(data, type, "revenue");
+                createGraphData(data, type, "downloads");
+
+                loadChart(historyList[selectedGraph]);
                 
             }
     });
+}
+
+function createGraphData(data, type, graphType) {
+
+    var rankData = [];
+    // var dateData = [];
+    var minPosition = 1000;
+    var maxPosition = 20;
+    var pos = 0;
+    var rangeData = [];
+    var rangeTop = 0;
+    var rangeBottom = 0;
+    var nodeDate;
+    var dayOfMonth = 0; // the current day of the month. i only want to show 1 entry per day
+    var dayOfMonthOld = -1;
+
+    $.each(data.ranks, function(index, item){    
+        // only use the specific "type" of list e.g. topfreeapplications, topfreeipadapplications
+        //if (item.type == type) {
+            
+            var nodeDate = new Date(item.date);
+            // dateData.push(nodeDate); // this is purely for the console.log call below
+
+            dayOfMonth = nodeDate.getDate();
+
+            if (dayOfMonth != dayOfMonthOld) {
+             
+                if (graphType == "rank") {
+                    // do things a bit differently if a top grossing app
+                    if ((type.indexOf("grossing") != -1)) {
+                        pos = item.grossingPosition;
+                    } else {
+                        pos = item.position;
+                    }
+                    rangeTop = pos;
+                    rangeBottom = pos;
+                }
+                else if (graphType == "revenue") {
+                    pos = item.revenue;
+                    rangeTop = pos - 5;
+                    rangeBottom = pos + 5;
+                }
+                else /*if (graphType == "downloads")*/ {
+                    pos = item.downloads;
+                    rangeTop = pos - 5;
+                    rangeBottom = pos + 5;
+                }
+                
+                    
+                var thisEntry = [item.date, pos];
+                rankData.push(thisEntry); // store the dates
+
+                rangeData.push([item.date, rangeTop, rangeBottom]);
+                
+
+                if (pos > maxPosition) {
+                    maxPosition = pos;
+                }
+
+                if (pos < minPosition) {
+                    minPosition = pos;
+                }
+            
+                dayOfMonthOld = dayOfMonth;
+            }
+        //};       
+    });
+
+    console.log(rankData);
+    // console.log(dateData);
+
+    maxPosition += 10;
+
+    if (minPosition < 20) {
+        minPosition = 1;
+    }
+    else  {
+        minPosition -= 10;
+    }
+
+    var history = {};
+    history.data = rankData;
+    history.rangeData = rangeData;
+    history.minPosition = minPosition;
+    history.maxPosition = maxPosition;
+    var heading;
+    var seriesName;
+    if (graphType == "rank") {
+        heading = "Rank History"
+        seriesName = "Rank";
+    }
+    else if (graphType == "revenue") {
+       heading = "Revenue History";
+       seriesName = "Revenue";
+    }
+    else /*if (graphType == "downloads")*/ {
+        heading = "Downloads History";
+        seriesName = "Downloads";
+    }
+    history.heading = heading;
+    history.seriesName = seriesName;
+
+    historyList[graphType] = history;
+    // console.log(historyList);
+
 }
 
 function updateApp(item) {
@@ -196,100 +235,81 @@ function updateApp(item) {
 
 }
 
-function loadChart(averagesData, minPosition, maxPosition) {
+function loadChart(graphData) {
 
-    /*var averagesData = [
-            [1246406400000, 21.5],
-            [1246492800000, 22.1],
-            [1246579200000, 23],
-            [1246665600000, 23.8],
-            [1246752000000, 21.4],
-            [1246838400000, 21.3],
-            [1246924800000, 18.3],
-            [1247011200000, 15.4],
-            [1247097600000, 16.4],
-            [1247184000000, 17.7],
-            [1247270400000, 17.5],
-            [1247356800000, 17.6],
-            [1247443200000, 17.7],
-            [1247529600000, 16.8],
-            [1247616000000, 17.7],
-            [1247702400000, 16.3],
-            [1247788800000, 17.8],
-            [1247875200000, 18.1],
-            [1247961600000, 17.2],
-            [1248048000000, 14.4],
-            [1248134400000, 13.7],
-            [1248220800000, 15.7],
-            [1248307200000, 14.6],
-            [1248393600000, 15.3],
-            [1248480000000, 15.3],
-            [1248566400000, 15.8],
-            [1248652800000, 15.2],
-            [1248739200000, 14.8],
-            [1248825600000, 14.4],
-            [1248912000000, 15],
-            [1248998400000, 13.6]
-        ];*/
+    var pointData = graphData.data;
+    var rangeData = graphData.rangeData;
+    var minPosition = graphData.minPosition;
+    var maxPosition = graphData.maxPosition;
+    var heading = graphData.heading;
+    var seriesName = graphData.seriesName;
 
-        $('#graph').show();
+    console.log(rangeData);
 
-        chart = new Highcharts.Chart({
-            chart: {
-                renderTo: 'graph',
-                //defaultSeriesType: 'spline',
+
+    $('#graph').show();
+
+    chart = new Highcharts.Chart({
+        chart: {
+            renderTo: 'graph',
+            //defaultSeriesType: 'spline',
+        },
+        title: {
+                text: heading,
+                x: -20 //center
             },
-            title: {
-                    text: 'Rank History',
-                    x: -20 //center
+            /*
+            subtitle: {
+                text: 'Source: Reflection.io',
+                x: -20
+            },
+            */
+            xAxis: {
+                type: 'datetime'
+            },
+            
+            yAxis: {
+                title: {
+                    text: null
                 },
-                /*
-                subtitle: {
-                    text: 'Source: Reflection.io',
-                    x: -20
-                },
-                */
-                xAxis: {
-                    type: 'datetime'
-                },
-                
-                yAxis: {
-                    title: {
-                        text: null
-                    },
-                    reversed: true,
-                    min: minPosition,
-                    max: maxPosition
-                },
-                tooltip: {
-                    //valueSuffix: '°C'
-                },
-                legend: {
-                    layout: 'vertical',
-                    align: 'right',
-                    verticalAlign: 'middle',
-                    borderWidth: 0
-                },
-                series: [{
-                    name: 'Rank',
-                    data: averagesData
-                }/*, {
-                    name: 'New York',
-                    data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-                }, {
-                    name: 'Berlin',
-                    data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-                }, {
-                    name: 'London',
-                    data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-                }*/]
-        });
+                reversed: true,
+                min: minPosition,
+                max: maxPosition
+            },
+            tooltip: {
+                //valueSuffix: '°C'
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle',
+                borderWidth: 0
+            },
+            series: [{
+                name: seriesName,
+                data: pointData,
+                marker: {
+                    fillColor: 'white',
+                    lineWidth: 2,
+                    lineColor: Highcharts.getOptions().colors[0]
+                }
+            }, {
+                name: 'Range',
+                data: rangeData,
+                type: 'arearange',
+                lineWidth: 0,
+                linkedTo: ':previous',
+                color: Highcharts.getOptions().colors[0],
+                fillOpacity: 0.3,
+                zIndex: 0
+            }]
+    });
 
 }
 
-function refreshChart(averagesData, minPosition, maxPosition) {
+function refreshChart(rankData, minPosition, maxPosition) {
     
     // chart.series[0].yAxis.min = 15;
-    chart.series[0].setData(averagesData,true);
+    chart.series[0].setData(rankData,true);
     // chart.redraw();
 }
