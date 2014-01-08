12eca9769c0b2ee64f41b2cf3e1269e353d3c8ee
altimario <altimario@fcbadaa9-13bc-4346-a2d3-50fbbe9e6b1b>
2014-01-08 18:00:01 +0000

    dropped for the maven version

--- war/js/flexslider.js
+++
@@ -1,9 +0,0 @@
-// Can also be used with $(document).ready()
-$(window).load(function() {
-
-  $('.flexslider').flexslider({
-    animation: "slide",
-    itemWidth: 960,
-    slideshowSpeed: 4000
-  });
-});
