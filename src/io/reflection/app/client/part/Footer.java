0be4acb9f9546cab4f89897e44ea9637a48d229e
billy1380 <billy1380@fcbadaa9-13bc-4346-a2d3-50fbbe9e6b1b>
2014-01-23 15:54:54 +0000

    - implemented footer - added glyphs that Martin recommended

--- src/io/reflection/app/client/part/Footer.java
+++ src/io/reflection/app/client/part/Footer.java
@@ -14,7 +14,7 @@ import com.google.gwt.user.client.ui.Widget;
 
 /**
  * @author billy1380
- *
+ * 
  */
 public class Footer extends Composite {
 
@@ -22,17 +22,6 @@ public class Footer extends Composite {
 
 	interface FooterUiBinder extends UiBinder<Widget, Footer> {}
 
-	/**
-	 * Because this class has a default constructor, it can
-	 * be used as a binder template. In other words, it can be used in other
-	 * *.ui.xml files as follows:
-	 * <ui:UiBinder xmlns:ui="urn:ui:com.google.gwt.uibinder"
-	 *   xmlns:g="urn:import:**user's package**">
-	 *  <g:**UserClassName**>Hello!</g:**UserClassName>
-	 * </ui:UiBinder>
-	 * Note that depending on the widget that is used, it may be necessary to
-	 * implement HasHTML instead of HasText.
-	 */
 	public Footer() {
 		initWidget(uiBinder.createAndBindUi(this));
 	}
