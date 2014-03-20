8f3406e8791e18f56d89b41ce1ef402c6b0430fd
billy1380 <billy1380@fcbadaa9-13bc-4346-a2d3-50fbbe9e6b1b>
2014-03-20 00:36:07 +0000

    moved admin pages to a separate package

---
+++ src/io/reflection/app/client/page/admin/EmailTemplatePage.java
@@ -0,0 +1,131 @@
+//
+//  EmailTemplatePage.java
+//  storedata
+//
+//  Created by William Shakour (billy1380) on 18 Feb 2014.
+//  Copyright © 2014 Reflection.io Ltd. All rights reserved.
+//
+package io.reflection.app.client.page.admin;
+
+import io.reflection.app.client.controller.EmailTemplateController;
+import io.reflection.app.client.controller.EventController;
+import io.reflection.app.client.controller.NavigationController;
+import io.reflection.app.client.controller.NavigationController.Stack;
+import io.reflection.app.client.controller.ServiceConstants;
+import io.reflection.app.client.handler.NavigationEventHandler;
+import io.reflection.app.client.page.Page;
+import io.reflection.app.client.part.BootstrapGwtCellTable;
+import io.reflection.app.client.part.SimplePager;
+import io.reflection.app.datatypes.shared.EmailTemplate;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.TextColumn;
+import com.google.gwt.user.cellview.client.TextHeader;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * @author billy1380
+ * 
+ */
+public class EmailTemplatePage extends Page implements NavigationEventHandler {
+
+	private static EmailTemplatePageUiBinder uiBinder = GWT.create(EmailTemplatePageUiBinder.class);
+
+	interface EmailTemplatePageUiBinder extends UiBinder<Widget, EmailTemplatePage> {}
+
+	@UiField(provided = true) CellTable<EmailTemplate> emailTemplates = new CellTable<EmailTemplate>(ServiceConstants.SHORT_STEP_VALUE,
+			BootstrapGwtCellTable.INSTANCE);
+	@UiField(provided = true) SimplePager pager = new SimplePager(false, false);
+
+	public EmailTemplatePage() {
+		initWidget(uiBinder.createAndBindUi(this));
+
+		addColumns();
+		
+		EmailTemplateController.get().addDataDisplay(emailTemplates);
+		pager.setDisplay(emailTemplates);
+	}
+
+	public void addColumns() {
+		TextColumn<EmailTemplate> idColumn = new TextColumn<EmailTemplate>() {
+
+			@Override
+			public String getValue(EmailTemplate object) {
+				return object.id.toString();
+			}
+
+		};
+
+		TextHeader idHeader = new TextHeader("Id");
+		idHeader.setHeaderStyleNames("col-xs-1");
+		emailTemplates.addColumn(idColumn, idHeader);
+
+		TextColumn<EmailTemplate> subjectColumn = new TextColumn<EmailTemplate>() {
+
+			@Override
+			public String getValue(EmailTemplate object) {
+				return object.subject;
+			}
+
+		};
+
+		TextHeader subjectHeader = new TextHeader("Subject");
+		subjectHeader.setHeaderStyleNames("col-xs-1");
+		emailTemplates.addColumn(subjectColumn, subjectHeader);
+
+		TextColumn<EmailTemplate> formatColumn = new TextColumn<EmailTemplate>() {
+
+			@Override
+			public String getValue(EmailTemplate object) {
+				return object.format.toString();
+			}
+
+		};
+
+		TextHeader formatHeader = new TextHeader("Format");
+		formatHeader.setHeaderStyleNames("col-xs-1");
+		emailTemplates.addColumn(formatColumn, formatHeader);
+
+		TextColumn<EmailTemplate> typeColumn = new TextColumn<EmailTemplate>() {
+
+			@Override
+			public String getValue(EmailTemplate object) {
+				return object.type.toString();
+			}
+
+		};
+
+		TextHeader typeHeader = new TextHeader("Type");
+		typeHeader.setHeaderStyleNames("col-xs-1");
+		emailTemplates.addColumn(typeColumn, typeHeader);
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see io.reflection.app.client.handler.NavigationEventHandler#navigationChanged(io.reflection.app.client.controller.NavigationController.Stack)
+	 */
+	@Override
+	public void navigationChanged(Stack stack) {
+		if ("emailtemplate".equals(stack.getPage()) && "view".equals(stack.getAction())) {
+			
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.google.gwt.user.client.ui.Composite#onAttach()
+	 */
+	@Override
+	protected void onAttach() {
+		super.onAttach();
+
+		register(EventController.get().addHandlerToSource(NavigationEventHandler.TYPE, NavigationController.get(), this));
+	}
+
+}
---
+++ src/io/reflection/app/client/page/admin/FeedBrowserPage.java
@@ -0,0 +1,213 @@
+//
+//  FeedBrowserPage.java
+//  storedata
+//
+//  Created by William Shakour (billy1380) on 5 Oct 2013.
+//  Copyright © 2013 SPACEHOPPER STUDIOS LTD. All rights reserved.
+//
+package io.reflection.app.client.page.admin;
+
+import io.reflection.app.client.controller.EventController;
+import io.reflection.app.client.controller.FeedFetchController;
+import io.reflection.app.client.controller.FilterController;
+import io.reflection.app.client.controller.ServiceConstants;
+import io.reflection.app.client.handler.FilterEventHandler;
+import io.reflection.app.client.page.Page;
+import io.reflection.app.client.part.BootstrapGwtCellTable;
+import io.reflection.app.client.part.Breadcrumbs;
+import io.reflection.app.client.part.SimplePager;
+import io.reflection.app.datatypes.shared.FeedFetch;
+
+import java.util.Map;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.i18n.shared.DateTimeFormat;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.TextColumn;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.view.client.SelectionChangeEvent;
+import com.google.gwt.view.client.SelectionChangeEvent.Handler;
+import com.google.gwt.view.client.SingleSelectionModel;
+
+/**
+ * @author billy1380
+ * 
+ */
+public class FeedBrowserPage extends Page implements FilterEventHandler {
+
+	private static FeedBrowserPageUiBinder uiBinder = GWT.create(FeedBrowserPageUiBinder.class);
+
+	interface FeedBrowserPageUiBinder extends UiBinder<Widget, FeedBrowserPage> {}
+
+	@UiField(provided = true) CellTable<FeedFetch> mFeeds = new CellTable<FeedFetch>(ServiceConstants.SHORT_STEP_VALUE, BootstrapGwtCellTable.INSTANCE);
+	@UiField(provided = true) SimplePager mPager = new SimplePager(false, false);
+
+	@UiField ListBox mAppStore;
+	@UiField ListBox mListType;
+	@UiField ListBox mCountry;
+	@UiField ListBox category;
+
+	@UiField Button mIngest;
+	@UiField Button mModel;
+	@UiField Button mPredict;
+
+	@UiField Breadcrumbs mBreadcrumbs;
+
+	public FeedBrowserPage() {
+		initWidget(uiBinder.createAndBindUi(this));
+
+		addFeedColumns();
+
+		final SingleSelectionModel<FeedFetch> s = new SingleSelectionModel<FeedFetch>();
+		s.addSelectionChangeHandler(new Handler() {
+
+			@Override
+			public void onSelectionChange(SelectionChangeEvent event) {
+				FeedFetch selected = s.getSelectedObject();
+
+				if (selected != null) {
+					mIngest.removeStyleName("disabled");
+					mModel.removeStyleName("disabled");
+					mPredict.removeStyleName("disabled");
+				} else {
+					mIngest.addStyleName("disabled");
+					mModel.addStyleName("disabled");
+					mPredict.addStyleName("disabled");
+				}
+			}
+		});
+		mFeeds.setSelectionModel(s);
+
+		FeedFetchController.get().addDataDisplay(mFeeds);
+		mPager.setDisplay(mFeeds);
+
+		refreshBreadcrumbs();
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.google.gwt.user.client.ui.Composite#onAttach()
+	 */
+	@Override
+	protected void onAttach() {
+		super.onAttach();
+		
+		register(EventController.get().addHandlerToSource(FilterEventHandler.TYPE, FilterController.get(), this));
+	}
+	
+	/**
+	 * 
+	 */
+	private void refreshBreadcrumbs() {
+		mBreadcrumbs.clear();
+		mBreadcrumbs.push(mAppStore.getItemText(mAppStore.getSelectedIndex()), mCountry.getItemText(mCountry.getSelectedIndex()),
+				mListType.getItemText(mListType.getSelectedIndex()), "Feeds");
+	}
+
+	/**
+	 * 
+	 */
+	private void addFeedColumns() {
+
+		mFeeds.addColumn(new TextColumn<FeedFetch>() {
+
+			@Override
+			public String getValue(FeedFetch object) {
+				return object.code.toString();
+			}
+		}, "Code");
+
+		mFeeds.addColumn(new TextColumn<FeedFetch>() {
+
+			@Override
+			public String getValue(FeedFetch object) {
+				return DateTimeFormat.getFormat("yyyy-MM-dd a").format(object.date);
+			}
+		}, "Date");
+
+		mFeeds.addColumn(new TextColumn<FeedFetch>() {
+
+			@Override
+			public String getValue(FeedFetch object) {
+				return object.type;
+			}
+		}, "Type");
+	}
+
+	@UiHandler("mAppStore")
+	void onAppStoreValueChanged(ChangeEvent event) {
+		FilterController.get().setStore(mAppStore.getValue(mAppStore.getSelectedIndex()));
+	}
+
+	@UiHandler("mListType")
+	void onListTypeValueChanged(ChangeEvent event) {
+		FilterController.get().setListType(mListType.getValue(mListType.getSelectedIndex()));
+	}
+
+	@UiHandler("mCountry")
+	void onCountryValueChanged(ChangeEvent event) {
+		FilterController.get().setCountry(mCountry.getValue(mCountry.getSelectedIndex()));
+	}
+	
+	@UiHandler("category")
+	void onCategoryValueChanged(ChangeEvent event) {
+		FilterController.get().setCategory(Long.valueOf(category.getValue(category.getSelectedIndex())));
+	}
+
+	@UiHandler("mIngest")
+	void onIngestClicked(ClickEvent event) {
+		@SuppressWarnings("unchecked")
+		FeedFetch selected = ((SingleSelectionModel<FeedFetch>) mFeeds.getSelectionModel()).getSelectedObject();
+
+		if (selected != null) {
+			FeedFetchController.get().ingest(selected.code);
+		}
+	}
+
+	@UiHandler("mModel")
+	void onModelClicked(ClickEvent event) {
+		@SuppressWarnings("unchecked")
+		FeedFetch selected = ((SingleSelectionModel<FeedFetch>) mFeeds.getSelectionModel()).getSelectedObject();
+
+		if (selected != null) {
+			FeedFetchController.get().model(selected.code);
+		}
+	}
+
+	@UiHandler("mPredict")
+	void onPredictClicked(ClickEvent event) {
+		@SuppressWarnings("unchecked")
+		FeedFetch selected = ((SingleSelectionModel<FeedFetch>) mFeeds.getSelectionModel()).getSelectedObject();
+
+		if (selected != null) {
+			FeedFetchController.get().predict(selected.code);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see io.reflection.app.client.handler.FilterEventHandler#filterParamChanged(java.lang.String, java.lang.Object, java.lang.Object)
+	 */
+	@Override
+	public <T> void filterParamChanged(String name, T currentValue, T previousValue) {
+		refreshBreadcrumbs();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see io.reflection.app.client.handler.FilterEventHandler#filterParamsChanged(java.util.Map, java.util.Map)
+	 */
+	@Override
+	public void filterParamsChanged(Map<String, ?> currentValues, Map<String, ?> previousValues) {
+		refreshBreadcrumbs();
+	}
+}
