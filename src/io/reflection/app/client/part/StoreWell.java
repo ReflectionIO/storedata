3b98ed07e5dc597e19b3383b029b8e002cc0d827
billy1380 <william.shakour@gmail.com>
2014-05-21 13:02:08 +0100

    removed stock/autogenerated comments

--- src/io/reflection/app/client/part/StoreWell.java
+++ src/io/reflection/app/client/part/StoreWell.java
@@ -34,15 +34,10 @@ public class StoreWell extends Composite {
 	interface StoreWellUiBinder extends UiBinder<Widget, StoreWell> {}
 
 	@UiField Hyperlink mMore;
-	
+
 	@UiField HTMLPanel mMainStores;
 	@UiField HTMLPanel mMoreStores;
 
-	/**
-	 * Because this class has a default constructor, it can be used as a binder template. In other words, it can be used in other *.ui.xml files as follows:
-	 * <ui:UiBinder xmlns:ui="urn:ui:com.google.gwt.uibinder" xmlns:g="urn:import:**user's package**"> <g:**UserClassName**>Hello!</g:**UserClassName>
-	 * </ui:UiBinder> Note that depending on the widget that is used, it may be necessary to implement HasHTML instead of HasText.
-	 */
 	public StoreWell() {
 		initWidget(uiBinder.createAndBindUi(this));
 
--- src/io/reflection/app/client/part/myapps/LinkedAccountsWaiting.java
+++ src/io/reflection/app/client/part/myapps/LinkedAccountsWaiting.java
@@ -14,7 +14,7 @@ import com.google.gwt.user.client.ui.Widget;
 
 /**
  * @author stefanocapuzzi
- *
+ * 
  */
 public class LinkedAccountsWaiting extends Composite {
 
@@ -22,17 +22,6 @@ public class LinkedAccountsWaiting extends Composite {
 
 	interface LinkedAccountsWaitingUiBinder extends UiBinder<Widget, LinkedAccountsWaiting> {}
 
-	/**
-	 * Because this class has a default constructor, it can
-	 * be used as a binder template. In other words, it can be used in other
-	 * *.ui.xml files as follows:
-	 * <ui:UiBinder xmlns:ui="urn:ui:com.google.gwt.uibinder"
-	 *   xmlns:g="urn:import:**user's package**">
-	 *  <g:**UserClassName**>Hello!</g:**UserClassName>
-	 * </ui:UiBinder>
-	 * Note that depending on the widget that is used, it may be necessary to
-	 * implement HasHTML instead of HasText.
-	 */
 	public LinkedAccountsWaiting() {
 		initWidget(uiBinder.createAndBindUi(this));
 	}
