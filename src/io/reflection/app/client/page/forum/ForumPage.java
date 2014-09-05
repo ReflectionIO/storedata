ReflectionIO/storedata

commit 03a705c57d57aa5b1c13e93b4cffafafa282a56b

ForumPage, null pointer check given new changes.

src/io/reflection/app/client/page/forum/ForumPage.java
@@ -324,7 +324,9 @@ public class ForumPage extends Page implements NavigationEventHandler, GetForums
 				configureTitleAndSidePanel();
 			}
 
-			newTopicButton.setTargetHistoryToken(PageType.ForumTopicPageType.asTargetHistoryToken("new", selectedForumId.toString()));
+			if (selectedForumId != null) {
+				newTopicButton.setTargetHistoryToken(PageType.ForumTopicPageType.asTargetHistoryToken("new", selectedForumId.toString()));
+			}
 		}
 	}
 

commit eda98a89321d1e73feb0b3de4e5e493330f8c92a

ForumPage: Changed InlineHyperlink to button to get rid of weird border
...that showed up at around 03a705c
Caused by dynamically setting the href... for some strange reason.

src/io/reflection/app/client/page/forum/ForumPage.java
@@ -33,6 +33,7 @@ import java.util.Date;
 import com.google.gwt.cell.client.SafeHtmlCell;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.HeadingElement;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.safecss.shared.SafeStyles;
 import com.google.gwt.safecss.shared.SafeStylesUtils;
 import com.google.gwt.safehtml.client.SafeHtmlTemplates;
@@ -42,10 +43,12 @@ import com.google.gwt.safehtml.shared.SafeUri;
 import com.google.gwt.safehtml.shared.UriUtils;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.cellview.client.CellTable;
 import com.google.gwt.user.cellview.client.Column;
 import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.cellview.client.TextHeader;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.InlineHyperlink;
@@ -81,7 +84,7 @@ public class ForumPage extends Page implements NavigationEventHandler, GetForums
 	@UiField ForumSummarySidePanel forumSummarySidePanel;
 	@UiField HeadingElement titleText;
 
-	@UiField InlineHyperlink newTopicButton;
+	@UiField Button newTopicButton;
 
 	private Forum selectedForum;
 
@@ -237,6 +240,8 @@ public class ForumPage extends Page implements NavigationEventHandler, GetForums
 
 		forumSummarySidePanel.reset();
 		titleText.setInnerHTML("");
+		
+		newTopicButton.setEnabled(false);
 	}
 
 	/*
@@ -327,11 +332,17 @@ public class ForumPage extends Page implements NavigationEventHandler, GetForums
 				configureTitleAndSidePanel();
 			}
 
-			if (selectedForumId != null) {
-				newTopicButton.setTargetHistoryToken(PageType.ForumTopicPageType.asTargetHistoryToken("new", selectedForumId.toString()));
-			}
+			newTopicButton.setEnabled(true);
 		}
 	}
+	
+	@UiHandler("newTopicButton")
+    void newTopicClick(ClickEvent event) {
+	    if (selectedForumId != null) {
+	        PageType.ForumTopicPageType.show("new", selectedForumId.toString()) ;
+	    }
+	}
+	
 
 	/*
 	 * (non-Javadoc)

src/io/reflection/app/client/page/forum/ForumPage.ui.xml
@@ -37,10 +37,12 @@
 			<g:HTMLPanel styleName="well {style.toolbar_spacing}">
 				<div class="row">
 					<h5 class="col-sm-8 {style.header2}" ui:field="titleText"></h5>
-					<g:InlineHyperlink styleName="btn btn-primary pull-right {style.buttonWidth}" ui:field="newTopicButton">
+					<!-- <g:InlineHyperlink styleName="btn btn-primary col-sm-2 pull-right {style.buttonWidth}" ui:field="newTopicButton"> -->
+					<g:Button ui:field="newTopicButton" styleName="btn btn-primary col-sm-2 pull-right">
 						New Topic
 						<span class="icon-plus"></span>
-					</g:InlineHyperlink>
+					</g:Button>
+					<!-- </g:InlineHyperlink> -->
 				</div>
 			</g:HTMLPanel>
 