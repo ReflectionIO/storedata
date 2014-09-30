6141c498a4bee4265faeb348a823bc553b4667f0
stefanocapuzzi <stefano@reflection.io>
2014-09-30 11:21:24 +0100

    User Permissions and Roles Providers

--- src/io/reflection/app/client/dataprovider/UserPermissionsProvider.java
+++ src/io/reflection/app/client/dataprovider/UserPermissionsProvider.java
@@ -7,16 +7,10 @@
 //
 package io.reflection.app.client.dataprovider;
 
-import io.reflection.app.client.controller.SessionController;
 import io.reflection.app.datatypes.shared.Permission;
-import io.reflection.app.datatypes.shared.User;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import com.google.gwt.view.client.AsyncDataProvider;
 import com.google.gwt.view.client.HasData;
-import com.google.gwt.view.client.Range;
 
 /**
  * @author Stefano Capuzzi (stefanocapuzzi)
@@ -24,42 +18,11 @@ import com.google.gwt.view.client.Range;
  */
 public class UserPermissionsProvider extends AsyncDataProvider<Permission> {
 
-	private User user;
-	private List<Permission> userPermissions = new ArrayList<Permission>();
-
-	/**
-	 * 
-	 */
-	public UserPermissionsProvider(User user) {
-		this.user = user;
-	}
-
 	/*
 	 * (non-Javadoc)
 	 * 
 	 * @see com.google.gwt.view.client.AbstractDataProvider#onRangeChanged(com.google.gwt.view.client.HasData)
 	 */
 	@Override
-	protected void onRangeChanged(HasData<Permission> display) {
-
-		Range r = display.getVisibleRange();
-
-		int start = r.getStart();
-		int end = start + r.getLength();
-
-		if (end > userPermissions.size()) {
-			if (SessionController.get().isLoggedInUserAdmin()) {
-				// UserController.get().fetchUserRolesAndPermissions(user); TODO
-			} else {
-				List<Permission> currentUserPermissions = SessionController.get().getLoggedInUser().permissions;
-				if (currentUserPermissions != null) {
-					updateRowData(0, currentUserPermissions);
-				} else {
-					updateRowCount(0, true);
-				}
-			}
-		} else {
-			updateRowData(start, userPermissions.subList(start, end));
-		}
-	}
+	protected void onRangeChanged(HasData<Permission> display) {}
 }
--- src/io/reflection/app/client/dataprovider/UserRolesProvider.java
+++ src/io/reflection/app/client/dataprovider/UserRolesProvider.java
@@ -7,17 +7,10 @@
 //
 package io.reflection.app.client.dataprovider;
 
-import io.reflection.app.client.controller.SessionController;
-import io.reflection.app.client.controller.UserController;
 import io.reflection.app.datatypes.shared.Role;
-import io.reflection.app.datatypes.shared.User;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import com.google.gwt.view.client.AsyncDataProvider;
 import com.google.gwt.view.client.HasData;
-import com.google.gwt.view.client.Range;
 
 /**
  * @author Stefano Capuzzi (stefanocapuzzi)
@@ -25,42 +18,12 @@ import com.google.gwt.view.client.Range;
  */
 public class UserRolesProvider extends AsyncDataProvider<Role> {
 
-	private User user;
-	private List<Role> userRoles = new ArrayList<Role>();
-
-	/**
-	 * 
-	 */
-	public UserRolesProvider(User user) {
-		this.user = user;
-	}
-
 	/*
 	 * (non-Javadoc)
 	 * 
 	 * @see com.google.gwt.view.client.AbstractDataProvider#onRangeChanged(com.google.gwt.view.client.HasData)
 	 */
 	@Override
-	protected void onRangeChanged(HasData<Role> display) {
-		Range r = display.getVisibleRange();
-
-		int start = r.getStart();
-		int end = start + r.getLength();
-
-		if (end > userRoles.size()) {
-			if (SessionController.get().isLoggedInUserAdmin()) {
-				UserController.get().fetchUserRolesAndPermissions(user);
-			} else {
-				List<Role> currentUserRoles = SessionController.get().getLoggedInUser().roles;
-				if (currentUserRoles != null) {
-					updateRowData(0, currentUserRoles);
-				} else {
-					updateRowCount(0, true);
-				}
-			}
-		} else {
-			updateRowData(start, userRoles.subList(start, end));
-		}
-	}
+	protected void onRangeChanged(HasData<Role> display) {}
 
 }
