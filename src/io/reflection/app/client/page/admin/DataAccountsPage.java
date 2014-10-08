194330df5e53e3fd5b08f63a98cd71ca3ab1b383
stefanocapuzzi <stefano@reflection.io>
2014-10-08 19:01:03 +0100

    DataAccountPage

--- src/io/reflection/app/client/page/admin/DataAccountsPage.java
+++ src/io/reflection/app/client/page/admin/DataAccountsPage.java
@@ -14,11 +14,18 @@ import io.reflection.app.client.part.BootstrapGwtCellTable;
 import io.reflection.app.client.part.SimplePager;
 import io.reflection.app.client.res.Images;
 import io.reflection.app.datatypes.shared.DataAccount;
+import io.reflection.app.shared.util.FormattingHelper;
 
+import com.google.gwt.cell.client.SafeHtmlCell;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.resources.client.CssResource;
+import com.google.gwt.safehtml.shared.SafeHtml;
+import com.google.gwt.safehtml.shared.SafeHtmlUtils;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.Column;
 import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Widget;
@@ -32,6 +39,13 @@ public class DataAccountsPage extends Page {
 	private static DataAccountsPageUiBinder uiBinder = GWT.create(DataAccountsPageUiBinder.class);
 
 	interface DataAccountsPageUiBinder extends UiBinder<Widget, DataAccountsPage> {}
+
+	interface DataAccountsPageStyle extends CssResource {
+		String green();
+
+	}
+
+	@UiField DataAccountsPageStyle style;
 
 	@UiField(provided = true) CellTable<DataAccount> dataAccountTable = new CellTable<DataAccount>(ServiceConstants.STEP_VALUE, BootstrapGwtCellTable.INSTANCE);
 	@UiField(provided = true) SimplePager simplePager = new SimplePager(false, false);
@@ -48,6 +62,37 @@ public class DataAccountsPage extends Page {
 
 	public void addColumns() {
 
+		TextColumn<DataAccount> idColumn = new TextColumn<DataAccount>() {
+
+			@Override
+			public String getValue(DataAccount object) {
+				return object.id.toString();
+			}
+
+		};
+		dataAccountTable.addColumn(idColumn, "id");
+
+		final DateTimeFormat dtf = DateTimeFormat.getFormat(FormattingHelper.DATE_FORMAT_DD_MMM_YYYY);
+		TextColumn<DataAccount> createdColumn = new TextColumn<DataAccount>() {
+
+			@Override
+			public String getValue(DataAccount object) {
+				return dtf.format(object.created);
+			}
+
+		};
+		dataAccountTable.addColumn(createdColumn, "Created");
+
+		TextColumn<DataAccount> userUsernameColumn = new TextColumn<DataAccount>() {
+
+			@Override
+			public String getValue(DataAccount object) {
+				return object.user != null && object.user.username != null ? object.user.username : "-";
+			}
+
+		};
+		dataAccountTable.addColumn(userUsernameColumn, "User username");
+
 		TextColumn<DataAccount> usernameColumn = new TextColumn<DataAccount>() {
 
 			@Override
@@ -56,7 +101,50 @@ public class DataAccountsPage extends Page {
 			}
 
 		};
-		dataAccountTable.addColumn(usernameColumn, "Username");
+		dataAccountTable.addColumn(usernameColumn, "Account username");
+
+		TextColumn<DataAccount> sourceColumn = new TextColumn<DataAccount>() {
+
+			@Override
+			public String getValue(DataAccount object) {
+				return object.source.name != null ? object.source.name : "-";
+			}
+
+		};
+		dataAccountTable.addColumn(sourceColumn, "Source");
+
+		Column<DataAccount, SafeHtml> activeColumn = new Column<DataAccount, SafeHtml>(new SafeHtmlCell()) {
+
+			@Override
+			public SafeHtml getValue(DataAccount object) {
+				return object.active.equals("y") ? SafeHtmlUtils.fromSafeConstant("<span class=\"icon-ok " + style.green() + "\"></span>")
+						: SafeHtmlUtils.EMPTY_SAFE_HTML;
+			}
+
+		};
+		dataAccountTable.addColumn(activeColumn, "Active");
+
+		TextColumn<DataAccount> fetchErrorsColumn = new TextColumn<DataAccount>() {
+
+			@Override
+			public String getValue(DataAccount object) {
+				return object.dataAccountFetches != null ? String.valueOf(object.dataAccountFetches.size()) : "";
+			}
+
+		};
+		dataAccountTable.addColumn(fetchErrorsColumn, "Fetch errors");
+
+		Column<DataAccount, SafeHtml> fetchColumn = new Column<DataAccount, SafeHtml>(new SafeHtmlCell()) {
+
+			@Override
+			public SafeHtml getValue(DataAccount object) {
+				// String id = object.id.toString();
+				// return SafeHtmlUtils.fromTrustedString("<a href=\"" + PageType.EmailTemplatesPageType.asHref(id).asString() + "\">Fetches</a>"); TODO
+				return SafeHtmlUtils.fromTrustedString("<a href=\"\">Fetches</a>");
+			}
+
+		};
+		dataAccountTable.addColumn(fetchColumn);
 
 	}
 }
