0b09668ad837f8e474785c941099d213e022777f
stefanocapuzzi <stefano@reflection.io>
2015-02-20 22:59:53 +0000

    stash

--- src/io/reflection/app/accountdatacollectors/DataAccountCollectorITunesConnect.java
+++ src/io/reflection/app/accountdatacollectors/DataAccountCollectorITunesConnect.java
@@ -12,12 +12,21 @@ import io.reflection.app.api.shared.ApiError;
 import io.reflection.app.datatypes.shared.DataAccount;
 import io.reflection.app.datatypes.shared.DataAccountFetch;
 import io.reflection.app.datatypes.shared.DataAccountFetchStatusType;
+import io.reflection.app.datatypes.shared.Event;
+import io.reflection.app.datatypes.shared.Notification;
+import io.reflection.app.datatypes.shared.User;
 import io.reflection.app.helpers.ApiHelper;
 import io.reflection.app.logging.GaeLevel;
 import io.reflection.app.service.dataaccountfetch.DataAccountFetchServiceProvider;
+import io.reflection.app.service.event.EventServiceProvider;
+import io.reflection.app.service.notification.NotificationServiceProvider;
+import io.reflection.app.service.user.UserServiceProvider;
+import io.reflection.app.shared.util.DataTypeHelper;
 
 import java.util.Date;
 import java.util.logging.Logger;
+
+import co.spchopr.persistentmap.PersistentMapFactory;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -125,6 +134,7 @@ public class DataAccountCollectorITunesConnect implements DataAccountCollector {
 				}
 
 				dataAccountFetch.data = error;
+
 			} else if (cloudFileName != null) {
 				dataAccountFetch.status = DataAccountFetchStatusType.DataAccountFetchStatusTypeGathered;
 				dataAccountFetch.data = cloudFileName;
@@ -145,6 +155,39 @@ public class DataAccountCollectorITunesConnect implements DataAccountCollector {
 				// once the data is collected
 				DataAccountFetchServiceProvider.provide().triggerDataAccountFetchIngest(dataAccountFetch);
 			}
+
+			if (error != null) {
+				if (PersistentMapFactory.createObjectify().get(dataAccount.id.toString()) == null) {
+					if (error.equals(DataTypeHelper.SALES_GATHER_CREDENTIAL_ERROR_EVENT_CODE)) {
+						PersistentMapFactory.createObjectify().put(dataAccount.id.toString(),
+								DataTypeHelper.SALES_GATHER_CREDENTIAL_ERROR_EVENT_CODE + ":first");
+					} else {
+						PersistentMapFactory.createObjectify().put(dataAccount.id.toString(), error);
+					}
+				} else if (PersistentMapFactory.createObjectify().get(dataAccount.id.toString())
+						.equals(DataTypeHelper.SALES_GATHER_CREDENTIAL_ERROR_EVENT_CODE + ":first")) {
+					// User dataAccountOwner = UserServiceProvider.provide().getDataAccountOwner(dataAccount);
+					// Store in datastore the last fetch result for this data account
+					// if (PersistentMapFactory.createObjectify().get(dataAccount.id.toString() == nu)
+					// .equals("Error :Your AppleConnect account or password was entered incorrectly.")) {}
+					// Error :Your AppleConnect account or password was entered incorrectly.
+				} else {
+
+				}
+				User adminUser = UserServiceProvider.provide().getUsernameUser("chi@reflection.io");
+				Event event = null;
+				if (error.equals("Error :Your AppleConnect account or password was entered incorrectly.")) {
+					// event = EventServiceProvider.provide().getCodeEvent(DataTypeHelper.SALES_GATHER_CREDENTIAL_ERROR_EVENT_CODE);
+				} else {
+					// event = EventServiceProvider.provide().getCodeEvent(DataTypeHelper.SALES_GATHER_GENERIC_ERROR_EVENT_CODE);
+				}
+				// Notify admin about the gather error
+				Notification notificationAdmin = (new Notification()).event(event).user(adminUser);
+				NotificationServiceProvider.provide().addNotification(notificationAdmin);
+			} else {
+				PersistentMapFactory.createObjectify().put(dataAccount.id.toString(), null);
+			}
+
 		} else {
 			LOG.warning(String.format("Gather for data account [%s] and date [%s] skipped because of status [%s]",
 					dataAccount.id == null ? dataAccount.username : dataAccount.id.toString(), dateParameter, dataAccountFetch.status));
@@ -152,7 +195,6 @@ public class DataAccountCollectorITunesConnect implements DataAccountCollector {
 
 		return success;
 	}
-
 	// /**
 	// * THIS CODE HAS TO BE REMOVED AFTER THE CLOSED BETA
 	// *
