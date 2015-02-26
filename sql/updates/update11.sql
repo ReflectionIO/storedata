1cfa4dab7d94fab0cd2a6d2285da9c2e747d7080
William Shakour <william.shakour@gmail.com>
2015-02-26 12:39:10 +0000

    update11 defaults to '' instead of '_'

--- sql/updates/update11.sql
+++ sql/updates/update11.sql
@@ -4,6 +4,6 @@ delimiter $$
 USE `rio`$$
 
 ALTER TABLE `rio`.`post` 
-ADD COLUMN `code` VARCHAR(45) NOT NULL DEFAULT '_' AFTER `title`,
+ADD COLUMN `code` VARCHAR(45) NOT NULL DEFAULT '' AFTER `title`,
 ADD INDEX `indexcode` (`code` ASC)$$
 
