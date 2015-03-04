1effb14137ce88d5fad5886690861f9ff6665a75
billy1380 <william.shakour@gmail.com>
2015-03-04 14:13:45 +0000

    fixed terminating characters

--- sql/updates/update10.sql
+++ sql/updates/update10.sql
@@ -6,5 +6,5 @@ ALTER TABLE `simplemodelrun`
 ADD COLUMN `astandarderror` DOUBLE NULL AFTER `b`,
 ADD COLUMN `bstandarderror` DOUBLE NULL AFTER `astandarderror`,
 ADD COLUMN `adjustedrsquared` DOUBLE NULL AFTER `bstandarderror`,
-ADD COLUMN `regressionsumsquares` DOUBLE NULL AFTER `adjustedrsquared`;
+ADD COLUMN `regressionsumsquares` DOUBLE NULL AFTER `adjustedrsquared`$$
 
