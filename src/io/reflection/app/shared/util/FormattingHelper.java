888a8742f342b5adf9a6aee23e055c3d123b8993
capuzzistefano <stefano@reflection.io>
2015-04-27 18:37:07 +0100

    Shared - Util

--- src/io/reflection/app/shared/util/FormattingHelper.java
+++ src/io/reflection/app/shared/util/FormattingHelper.java
@@ -25,21 +25,51 @@ public class FormattingHelper {
 	// public static final String DATE_FORMAT_YYYY_MM_DD = "yyyy-MM-dd";
 	public static final String DATE_FORMAT_DD_MMM_YYYY_HH_MM = "dd MMM yyyy - HH:mm";
 	
-	public static final String MONEY_FORMAT = ",###.##";
+	public static final String TWO_DECIMALS_FORMAT = ",###.##";
 	public static final String WHOLE_NUMBER_FORMAT = ",###";
 
 	private static Map<String, String> currencySymbolLookup = null;
+	private static Map<String, String> countryCurrencyLookup = null;
 
-	private static void setup() {
-		if (currencySymbolLookup == null) {
+	static {
 			currencySymbolLookup = new HashMap<String, String>();
 
 			currencySymbolLookup.put("USD", "$");
 			currencySymbolLookup.put("EUR", "€");
 			currencySymbolLookup.put("CNY", "¥");
 			currencySymbolLookup.put("GBP", "£");
+
+		countryCurrencyLookup = new HashMap<String, String>();
+
+		countryCurrencyLookup.put("US", "USD");
+
+		countryCurrencyLookup.put("AT", "EUR");
+		countryCurrencyLookup.put("BE", "EUR");
+		countryCurrencyLookup.put("CY", "EUR");
+		countryCurrencyLookup.put("DE", "EUR");
+		countryCurrencyLookup.put("EE", "EUR");
+		countryCurrencyLookup.put("ES", "EUR");
+		countryCurrencyLookup.put("FI", "EUR");
+		countryCurrencyLookup.put("FR", "EUR");
+		countryCurrencyLookup.put("GR", "EUR");
+		countryCurrencyLookup.put("IE", "EUR");
+		countryCurrencyLookup.put("IT", "EUR");
+		countryCurrencyLookup.put("LT", "EUR");
+		countryCurrencyLookup.put("LU", "EUR");
+		countryCurrencyLookup.put("LV", "EUR");
+		countryCurrencyLookup.put("MC", "EUR");
+		countryCurrencyLookup.put("MT", "EUR");
+		countryCurrencyLookup.put("NL", "EUR");
+		countryCurrencyLookup.put("PT", "EUR");
+		countryCurrencyLookup.put("SI", "EUR");
+		countryCurrencyLookup.put("SK", "EUR");
+		countryCurrencyLookup.put("SM", "EUR");
+		countryCurrencyLookup.put("VA", "EUR");
+
+		countryCurrencyLookup.put("CN", "CNY");
+		countryCurrencyLookup.put("GB", "GBP");
+
 		}
-	}
 
 	/**
 	 * Returns a currency sumbol or code if none are found
@@ -48,13 +78,15 @@ public class FormattingHelper {
 	 * @return
 	 */
 	public static String getCurrencySymbol(String currency) {
-		if (currencySymbolLookup == null) {
-			setup();
-		}
-
 		String symbol = currencySymbolLookup.get(currency);
 
 		return symbol == null ? currency : symbol;
+		}
+
+	public static String getCountryCurrency(String country) {
+		String currency = countryCurrencyLookup.get(country);
+
+		return currency == null ? "" : currency;
 	}
 
 	public static String getUserName(User user) {
@@ -129,5 +161,9 @@ public class FormattingHelper {
 		}
 
 		return timeSince;
+	}
+
+	public static String wordEmail(String address) {
+		return address == null ? "empty" : address.replace(".", " dot ").replace("@", " at ");
 	}
 }
