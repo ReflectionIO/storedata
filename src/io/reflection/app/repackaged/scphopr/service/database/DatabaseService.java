6225fd776a059d081edc998c02c05be04fbf13ca
Mitul Amin <mitul@reflection.io>
2015-05-10 11:04:06 +0100

    DB Connections are not reused through out a request and only really closed at the end of the request

--- src/io/reflection/app/repackaged/scphopr/service/database/DatabaseService.java
+++ src/io/reflection/app/repackaged/scphopr/service/database/DatabaseService.java
@@ -13,9 +13,11 @@ import io.reflection.app.service.ServiceType;
 
 /**
  * @author William Shakour
- * 
+ *
  */
 public class DatabaseService implements IDatabaseService {
+
+	private static final ThreadLocal<Connection> localConnection = new ThreadLocal<Connection>();
 
 	@Override
 	public String getName() {
@@ -24,14 +26,20 @@ public class DatabaseService implements IDatabaseService {
 
 	@Override
 	public Connection getConnection(String server, String database, String username, String password) throws DataAccessException {
-		return new Connection(server, database, username, password);
+		Connection connection = localConnection.get();
+		if (connection == null) {
+			connection = new Connection(server, database, username, password);
+			localConnection.set(connection);
+		}
+
+		return connection;
 	}
 
 	@Override
 	public Connection getNamedConnection(String name) throws DataAccessException {
 		Connection connection = null;
 
-		String server = System.getProperty(DATABASE_SERVER_KEY), database = System.getProperty(DATABASE_CATALOGURE_KEY), username = System
+		final String server = System.getProperty(DATABASE_SERVER_KEY), database = System.getProperty(DATABASE_CATALOGURE_KEY), username = System
 				.getProperty(DATABASE_USERNAME_KEY), password = System.getProperty(DATABASE_PASSWORD_KEY);
 		connection = getConnection(server, database, username, password);
 
@@ -42,7 +50,7 @@ public class DatabaseService implements IDatabaseService {
 	public Connection getAdminNamedConnection(String name) throws DataAccessException {
 		Connection connection = null;
 
-		String server = System.getProperty(DATABASE_SERVER_KEY), database = System.getProperty(DATABASE_CATALOGURE_KEY), username = System
+		final String server = System.getProperty(DATABASE_SERVER_KEY), database = System.getProperty(DATABASE_CATALOGURE_KEY), username = System
 				.getProperty(DATABASE_ADMIN_USERNAME_KEY), password = System.getProperty(DATABASE_ADMIN_PASSWORD_KEY);
 		connection = getConnection(server, database, username, password);
 
@@ -53,11 +61,27 @@ public class DatabaseService implements IDatabaseService {
 	public Connection getCronNamedConnection(String name) throws DataAccessException {
 		Connection connection = null;
 
-		String server = System.getProperty(DATABASE_SERVER_KEY), database = System.getProperty(DATABASE_CATALOGURE_KEY), username = System
+		final String server = System.getProperty(DATABASE_SERVER_KEY), database = System.getProperty(DATABASE_CATALOGURE_KEY), username = System
 				.getProperty(DATABASE_CRON_USERNAME_KEY), password = System.getProperty(DATABASE_CRON_PASSWORD_KEY);
 		connection = getConnection(server, database, username, password);
 
 		return connection;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see io.reflection.app.repackaged.scphopr.service.database.IDatabaseService#realDisconnect()
+	 */
+	@Override
+	public void realDisconnect() {
+		final Connection connection = localConnection.get();
+		if (connection == null) return;
+
+		try {
+			connection.realDisconnect();
+		} catch (final DataAccessException e) {
+			e.printStackTrace();
+		}
+	}
 }
