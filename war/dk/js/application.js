c3b1db5a8dd8324433e1fc31c3d0374e53c9db69
Jamie Gilman <jamie@reflection.io>
2015-06-02 16:58:27 +0100

    Picturefill fix for FF, github storedata bug #611

--- war/dk/js/application.js
+++ war/dk/js/application.js
@@ -18,6 +18,7 @@
 		new AccountContainer();
 		new SearchContainer();
 		this.customScrollbars();
+		this.pictureFillFix();
 	};
 
 	Page.prototype.customScrollbars = function() {
@@ -63,6 +64,55 @@
 	  });
 	};
 
+	Page.prototype.pictureFillFix = function() {
+		if($('.is-firefox').length) {
+			/**
+			 * FF's first picture implementation is static and does not react to viewport changes, this tiny script fixes this.
+			 */
+			var ua = navigator.userAgent;
+
+			if(window.HTMLPictureElement && ((/ecko/).test(ua) && ua.match(/rv\:(\d+)/) && RegExp.$1 < 41)){
+				addEventListener('resize', (function(){
+					var timer;
+
+					var dummySrc = document.createElement('source');
+
+					var fixPicture = function(img){
+						var picture = img.parentNode;
+						var source = dummySrc.cloneNode();
+						picture.insertBefore(source, picture.firstElementChild);
+						setTimeout(function(){
+							picture.removeChild(source);
+						});
+					};
+
+					var findPictureImgs = function(){
+						var i;
+						var imgs = document.querySelectorAll('picture > img');
+						for(i = 0; i < imgs.length; i++){
+							if(imgs[i].complete){
+								if(imgs[i].currentSrc){
+									fixPicture(imgs[i]);
+								}
+							} else if(imgs[i].currentSrc){
+								removeEventListener('resize', onResize);
+								break;
+							}
+						}
+					};
+					var onResize = function(){
+						clearTimeout(timer);
+						timer = setTimeout(findPictureImgs, 99);
+					};
+
+					dummySrc.srcset = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
+
+					return onResize;
+				})());
+			}
+		}
+	}
+
 
 // object for browser detection related functionality
 	var BrowserDetection = function() {
@@ -70,6 +120,7 @@
 		this.setChromeClass();
 		this.setOperaClass();
 		this.setSafariClass();
+		this.setFireFoxClass();
 	}
 
 	BrowserDetection.prototype.setIEClass = function() {
@@ -123,6 +174,11 @@
 	  // other browser
 	  return false;
 	};
+
+	BrowserDetection.prototype.setFireFoxClass = function() {
+		var is_firefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+		if(is_firefox) { $('html').addClass('is-firefox'); }
+	}
 
 
 /* COMPONENT OBJECTS */
