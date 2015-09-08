dadb20302c087820d1a5ad30b03c1286b44843b9
Jamie Gilman <jamie@reflection.io>
2015-09-08 16:06:47 +0100

    Left nav design updates (RWA-589) Content amend for Lorem ipsum pages

--- war/dk/js/application-global-modules-logged-in.js
+++ war/dk/js/application-global-modules-logged-in.js
@@ -2,7 +2,7 @@ var templateGlobalHeader = Handlebars.templates['globalHeaderLoggedIn'];
 var htmlGlobalHeader = templateGlobalHeader({});
 $("#js-component-import--global-header").html(htmlGlobalHeader);
 
-var templateAppMenu = Handlebars.templates['panelLeft'];
+var templateAppMenu = Handlebars.templates['appMenuLoggedIn'];
 var htmlAppMenu = templateAppMenu({});
 $("#js-component-import--panel-left").html(htmlAppMenu);
 
