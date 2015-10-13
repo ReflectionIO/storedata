552155f27d08cbe78a814efb213ffc73615bcc23
Jamie Gilman <jamie@reflection.io>
2015-10-13 17:21:44 +0100

    Content updates, banner image updates, minor style amends

--- src/io/reflection/app/client/res/Styles.java
+++ src/io/reflection/app/client/res/Styles.java
@@ -46,6 +46,9 @@ public interface Styles extends ClientBundle {
 		@ClassName("MHXTE6C-r-a")
 		String mhxte6cRA();
 
+		@ClassName("about-main-content")
+		String aboutMainContent();
+
 		String accordion();
 
 		@ClassName("accordion-bottom-with-divider")
@@ -928,15 +931,6 @@ public interface Styles extends ClientBundle {
 		@ClassName("interactions-page")
 		String interactionsPage();
 
-		@ClassName("intro-banner")
-		String introBanner();
-
-		@ClassName("intro-banner__graphic")
-		String introBanner__graphic();
-
-		@ClassName("intro-banner__intro")
-		String introBanner__intro();
-
 		@ClassName("is-activated")
 		String isActivated();
 
@@ -1530,11 +1524,11 @@ public interface Styles extends ClientBundle {
 		@ClassName("pricing-access-info--highlighted")
 		String pricingAccessInfoHighlighted();
 
+		@ClassName("pricing-detail-coming-soon")
+		String pricingDetailComingSoon();
+
 		@ClassName("pricing-details")
 		String pricingDetails();
-
-		@ClassName("pricing-details-with-info")
-		String pricingDetailsWithInfo();
 
 		@ClassName("pricing-name")
 		String pricingName();
@@ -2205,7 +2199,6 @@ public interface Styles extends ClientBundle {
 
 		@ClassName("window-warning")
 		String windowWarning();
-
 	}
 
 	public static final Styles STYLES_INSTANCE = GWT.create(Styles.class);
