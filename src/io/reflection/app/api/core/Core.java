ef7c5251afeab5af208b22cdd7bf9f594c621e50
Stefano Capuzzi (capuzzistefano) <Stefano Capuzzi (capuzzistefano)@192.168.0.7>
2016-01-08 16:20:28 +0100

    Allow data accounts from same apple id to be split between users

--- src/io/reflection/app/api/core/Core.java
+++ src/io/reflection/app/api/core/Core.java
@@ -1321,11 +1321,15 @@ public final class Core extends ActionHandler {
 				if (vendors.isEmpty()) throw new Exception("Vendor list is empty");
 
 				// Only Admin can add another Apple ID pointing to an existing linked account
-				if (!isAdmin) { // check if duplicate vendor Id exists and throw exception
-					for (String v : vendors) {
-						if (!DataAccountServiceProvider.provide().getVendorDataAccounts(v, Boolean.FALSE).isEmpty()) // At least 1 active account
-							throw new InputValidationException(ApiError.DuplicateVendorId.getCode(), ApiError.DuplicateVendorId.getMessage(input.username));
+				if (!isAdmin) { // check if ALL vendor Id are active, if so throw exception, otherwise link the one inactive
+					for (String v : new ArrayList<String>(vendors)) {
+						if (!DataAccountServiceProvider.provide().getVendorDataAccounts(v, Boolean.FALSE).isEmpty()) { // Get active account
+							vendors.remove(v);
+						}
 					}
+					if (vendors.isEmpty())
+						throw new InputValidationException(ApiError.DuplicateVendorId.getCode(),
+								ApiError.DuplicateVendorId.getMessage("All vendors for this Apple ID already linked"));
 				}
 
 				// Add a data account for every vendor
