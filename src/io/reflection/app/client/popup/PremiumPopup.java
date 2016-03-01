579b033c1c388fe14dd2e010af04e2cd18237fc0
Mitul Amin <mitul@reflection.io>
2016-03-01 21:53:06 +0000

    PremiumPopup reloads page on close if user upgraded and on leaderboard

--- src/io/reflection/app/client/popup/PremiumPopup.java
+++ src/io/reflection/app/client/popup/PremiumPopup.java
@@ -7,18 +7,6 @@
 //
 package io.reflection.app.client.popup;
 
-import io.reflection.app.api.core.shared.call.UpgradeAccountRequest;
-import io.reflection.app.api.core.shared.call.UpgradeAccountResponse;
-import io.reflection.app.api.core.shared.call.event.UpgradeAccountEventHandler;
-import io.reflection.app.client.DefaultEventBus;
-import io.reflection.app.client.component.LoadingButton;
-import io.reflection.app.client.controller.SessionController;
-import io.reflection.app.client.controller.UserController;
-import io.reflection.app.client.page.PageType;
-import io.reflection.app.client.res.Styles;
-import io.reflection.app.datatypes.shared.User;
-import io.reflection.app.shared.util.DataTypeHelper;
-
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.Style.Display;
@@ -27,12 +15,26 @@ import com.google.gwt.event.logical.shared.CloseEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.InlineHyperlink;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.willshex.gson.json.service.shared.StatusType;
+
+import io.reflection.app.api.core.shared.call.UpgradeAccountRequest;
+import io.reflection.app.api.core.shared.call.UpgradeAccountResponse;
+import io.reflection.app.api.core.shared.call.event.UpgradeAccountEventHandler;
+import io.reflection.app.client.DefaultEventBus;
+import io.reflection.app.client.component.LoadingButton;
+import io.reflection.app.client.controller.NavigationController;
+import io.reflection.app.client.controller.SessionController;
+import io.reflection.app.client.controller.UserController;
+import io.reflection.app.client.page.PageType;
+import io.reflection.app.client.res.Styles;
+import io.reflection.app.datatypes.shared.User;
+import io.reflection.app.shared.util.DataTypeHelper;
 
 /**
  * @author Stefano Capuzzi (capuzzistefano)
@@ -52,21 +54,25 @@ public class PremiumPopup extends Composite implements UpgradeAccountEventHandle
 	@UiField InlineHyperlink letUsKnow;
 	@UiField Button continueBrowsing;
 
+	private boolean userUpgraded = false;
+
 	public PremiumPopup() {
 		initWidget(uiBinder.createAndBindUi(this));
 
 	}
 
 	public void show(boolean isFeature) {
-		if (!this.asWidget().isAttached()) {
+		if (!asWidget().isAttached()) {
 			if (isFeature) {
 				badge.getStyle().setDisplay(Display.INLINE);
 				title.setInnerText("This is a Premium Feature");
-				subtitle.setInnerText("To use this feature click 'Start Free Trial' below and we'll upgrade your account to Developer Premium for free for the duration of the beta.");
+				subtitle.setInnerText(
+						"To use this feature click 'Start Free Trial' below and we'll upgrade your account to Developer Premium for free for the duration of the beta.");
 			} else {
 				badge.getStyle().setDisplay(Display.NONE);
 				title.setInnerText("Upgrade For Free");
-				subtitle.setInnerText("No extra details required, just confirm below and we’ll instantly upgrade your account to Developer Premium for the duration of the beta absolutely free.");
+				subtitle.setInnerText(
+						"No extra details required, just confirm below and we’ll instantly upgrade your account to Developer Premium for the duration of the beta absolutely free.");
 			}
 			popup.setStyleName(Styles.STYLES_INSTANCE.reflectionMainStyle().isSubmittedSuccess(), SessionController.get().isPremiumDeveloper());
 			RootPanel.get().add(this);
@@ -84,7 +90,13 @@ public class PremiumPopup extends Composite implements UpgradeAccountEventHandle
 
 	@UiHandler("popup")
 	void onPopupClosed(CloseEvent<PopupBase> event) {
-		RootPanel.get().remove(this.asWidget());
+		if (userUpgraded) {
+			if (NavigationController.get().getCurrentPage() == PageType.RanksPageType) {
+				Window.Location.reload();
+			}
+		}
+
+		RootPanel.get().remove(asWidget());
 	}
 
 	@UiHandler("startFreeTrial")
@@ -101,18 +113,23 @@ public class PremiumPopup extends Composite implements UpgradeAccountEventHandle
 		}
 	}
 
+	@UiHandler("continueBrowsing")
+	void onContinueBrowsingClicked(ClickEvent event) {
+		popup.closePopup();
+	}
+
 	/*
 	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * io.reflection.app.api.core.shared.call.event.UpgradeAccountEventHandler#upgradeAccountSuccess(io.reflection.app.api.core.shared.call.UpgradeAccountRequest
-	 * , io.reflection.app.api.core.shared.call.UpgradeAccountResponse)
+	 *
+	 * @see io.reflection.app.api.core.shared.call.event.UpgradeAccountEventHandler#upgradeAccountSuccess(io.reflection.app.api.core.shared.call.
+	 * UpgradeAccountRequest , io.reflection.app.api.core.shared.call.UpgradeAccountResponse)
 	 */
 	@Override
 	public void upgradeAccountSuccess(UpgradeAccountRequest input, UpgradeAccountResponse output) {
 		if (output.status == StatusType.StatusTypeSuccess) {
 			popup.setStyleName(Styles.STYLES_INSTANCE.reflectionMainStyle().isSubmittedSuccess(), true);
 			startFreeTrial.setStatusSuccess();
+			userUpgraded = true;
 		} else {
 			startFreeTrial.setStatusError();
 		}
@@ -120,10 +137,9 @@ public class PremiumPopup extends Composite implements UpgradeAccountEventHandle
 
 	/*
 	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * io.reflection.app.api.core.shared.call.event.UpgradeAccountEventHandler#upgradeAccountFailure(io.reflection.app.api.core.shared.call.UpgradeAccountRequest
-	 * , java.lang.Throwable)
+	 *
+	 * @see io.reflection.app.api.core.shared.call.event.UpgradeAccountEventHandler#upgradeAccountFailure(io.reflection.app.api.core.shared.call.
+	 * UpgradeAccountRequest , java.lang.Throwable)
 	 */
 	@Override
 	public void upgradeAccountFailure(UpgradeAccountRequest input, Throwable caught) {
